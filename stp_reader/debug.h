a34d80ed5455b1ba957660a82878ab658c2ae983
Gabriel Morete <gabrielmorete@usp.br>
2020-10-23 15:35:48 -0300

    test cases

---
+++ stp_reader/debug.h
@@ -0,0 +1,10 @@
+#define gnl cout << endl
+#define chapa cout << "oi meu chapa" << endl
+
+#define dbg(x)  cout << #x << " = " << x << endl
+#define all(x)  x.begin(),x.end()
+
+#define fr(i,n)     for (int i = 0; i < n; i++)
+#define frr(i,n)    for (int i = 1; i <= n; i++)
+
+const int INF = 0x3f3f3f3f;
